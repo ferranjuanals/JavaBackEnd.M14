cfc648e20ffc50fe23bde7249bf386fc84efccd2
Ferran Juanals <ferranjuanals@gmail.com>
2021-03-18 11:57:19 +0100

    Spring project

---
+++ exercici/src/main/java/springrestapi/exercici/ExerciciApplication.java
@@ -0,0 +1,13 @@
+package springrestapi.exercici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciApplication.class, args);
+	}
+
+}
