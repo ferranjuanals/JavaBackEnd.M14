efe92fda657add776f1b743a854ba05b67686a96
Ferran Juanals <ferranjuanals@gmail.com>
2021-04-05 20:09:50 +0200

    Exceptions

---
+++ exercici/src/main/java/springrestapi/exercici/exception/IdNotFoundException.java
@@ -0,0 +1,15 @@
+package springrestapi.exercici.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class IdNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public IdNotFoundException(long id) {
+		super("ID: " + id + "not found");
+	}
+
+}
